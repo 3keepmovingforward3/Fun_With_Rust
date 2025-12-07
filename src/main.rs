3828c894dc066f1e131f72089a8b6ab16ef18b6b
Benjamin Blouin <3keepmovingforward3@users.noreply.github.com>
2025-12-07 17:03:08 -0500

    Update main.rs

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,5 @@
-
+mod math;
+pub use math::my_math::is_prime;
 fn main() {
     //Let's find Mersenne Primes
     // M_n = 2^n - 1
@@ -10,20 +11,4 @@ fn main() {
             }
         }  
     }
-
-}
-
-fn is_prime(num: u32) -> bool {
-    if num <= 1 {
-        return false; // Numbers less than or equal to 1 are not prime
-    }
-    
-    // Check if num is divisible by any number from 2 to the square root of num
-    for i in 2..=(num as f64).sqrt() as u32 {
-        if num % i == 0 {
-            return false; // If num is divisible by any number other than 1 and itself, it's not prime
-        }
-    }
-    
-    true // If num is not divisible by any number other than 1 and itself, it's prime
 }
